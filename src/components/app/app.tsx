ca40224b5267c1cce70436e4ce46df132046caec
kamelot43 <andrepud@mail.ru>
2024-05-25 16:53:14 +0300

    module3-task1 добавление приватного маршрута

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,12 +3,13 @@ import Main from '../../pages/main/main';
 import {films} from '../../mocks/films';
 import {FilmCardProps} from '../film-card/film-card';
 import MyList from '../../pages/my-list/my-list';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import Player from '../../pages/player/player';
 import Film from '../../pages/film/film';
 import AddReview from '../../pages/add-review/add-review';
 import PageNotFound from '../../pages/page-not-found/page-not-found';
 import Login from '../../pages/login/login';
+import PrivateRoute from '../private-route/private-route';
 
 const filmCardMock: FilmCardProps = {
   title: 'The Grand Budapest Hotel',
@@ -22,13 +23,40 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Main films={films} filmCardProps={filmCardMock} />} />
-        <Route path={AppRoute.Login} element={<Login />} />
-        <Route path={AppRoute.MyList} element={<MyList films={films} />} />
-        <Route path={`${AppRoute.Player}/:id`} element={<Player />} />
-        <Route path={`${AppRoute.Film}/:id`} element={<Film films={films} />} />
-        <Route path={`${AppRoute.Film}/:id/${AppRoute.Review}`} element={<AddReview />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route
+          index
+          element={<Main films={films} filmCardProps={filmCardMock} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyList films={films} />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={`${AppRoute.Player}/:id`}
+          element={<Player />}
+        />
+        <Route
+          path={`${AppRoute.Film}/:id`}
+          element={<Film films={films} />}
+        />
+        <Route
+          path={`${AppRoute.Film}/:id/${AppRoute.Review}`}
+          element={<AddReview />}
+        />
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
       </Routes>
     </BrowserRouter>
   );
