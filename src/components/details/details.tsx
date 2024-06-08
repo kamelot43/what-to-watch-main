77b3b219a53a016ac552c0ee8c498dc91194d369
kamelot43 <andrepud@mail.ru>
2024-06-09 00:50:07 +0300

    module5-task2 добавил новые компоненты

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,10 +1,39 @@
 import {Film} from '../../types/film';
 
-export default function Details(data: Film) {
-  /* eslint-disable*/
-  console.log('data', data);
+export default function Details({starring, director, genre, released, runTime}: Film) {
   return (
-    <div>Details</div>
+    <div className="film-card__text film-card__row">
+      <div className="film-card__text-col">
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Director</strong>
+          <span className="film-card__details-value">{director}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Starring</strong>
+          <span className="film-card__details-value">
+            {starring.map((item) => (
+              <>
+                {item}<br/>
+              </>
+            ))}
+          </span>
+        </p>
+      </div>
+      <div className="film-card__text-col">
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Run Time</strong>
+          <span className="film-card__details-value">{runTime}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Genre</strong>
+          <span className="film-card__details-value">{genre}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Released</strong>
+          <span className="film-card__details-value">{released}</span>
+        </p>
+      </div>
+    </div>
   );
 }
 
--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,9 +1,72 @@
 import {Film} from '../../types/film';
 
 export default function Reviews(data: Film) {
-  /* eslint-disable*/
-  console.log('data', data);
   return (
-    <div>Reviews</div>
+    <>
+      <div className="film-card__reviews-col">
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director funniest and most exquisitely designed films in years.</p>
+            <footer className="review__details">
+              <cite className="review__author">Kate Muir</cite>
+              <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">8,9</div>
+        </div>
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">Anderson films are too precious for some, but for those of us willing to lose ourselves in them, theye a delight. The Grand Budapest Hotel is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+            <footer className="review__details">
+              <cite className="review__author">Bill Goodykoontz</cite>
+              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">8,0</div>
+        </div>
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">I didn find it amusing, and while I can appreciate the creativity, it an hour and 40 minutes I wish I could take back.</p>
+            <footer className="review__details">
+              <cite className="review__author">Amanda Greever</cite>
+              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">8,0</div>
+        </div>
+      </div>
+      <div className="film-card__reviews-col">
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director funniest and most exquisitely designed films in years.</p>
+            <footer className="review__details">
+              <cite className="review__author">Kate Muir</cite>
+              <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">8,9</div>
+        </div>
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">Anderson films are too precious for some, but for those of us willing to lose ourselves in them, theye a delight. The Grand Budapest Hotel is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+            <footer className="review__details">
+              <cite className="review__author">Bill Goodykoontz</cite>
+              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">8,0</div>
+        </div>
+        <div className="review">
+          <blockquote className="review__quote">
+            <p className="review__text">I didn find it amusing, and while I can appreciate the creativity, it an hour and 40 minutes I wish I could take back.</p>
+            <footer className="review__details">
+              <cite className="review__author">Amanda Greever</cite>
+              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+            </footer>
+          </blockquote>
+          <div className="review__rating">8,0</div>
+        </div>
+      </div>
+    </>
   );
 }
