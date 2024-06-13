8c8ddb0f5b5c825755a9109975acca0806dcb9c5
kamelot43 <andrepud@mail.ru>
2024-06-13 08:41:57 +0300

    module7-task2 доработал шапку (кроме хлебных крошек)

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,19 +1,11 @@
 // components
 import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
 
 export default function Login() {
   return (
     <div className="user-page">
-      <header className="page-header user-page__head">
-        <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </header>
+      <Header extraClass="user-page__head" title="Sign In" hideSignIn/>
       <div className="sign-in user-page__content">
         <form action="#" className="sign-in__form">
           <div className="sign-in__fields">
